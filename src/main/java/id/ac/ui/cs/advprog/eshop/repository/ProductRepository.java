e0f2d18f617249f84c692b9c1ae27c7d92d89056
steven-fo <stevenfo0711@gmail.com>
2024-02-06 10:01:18 +0700

    fix: fix product id null

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -10,8 +10,11 @@ import java.util.List;
 @Repository
 public class ProductRepository {
     private List<Product> productData = new ArrayList<>();
+    private int id = 1;
 
     public Product create(Product product) {
+        product.setProductId(Integer.toString(id));
+        id++;
         productData.add(product);
         return product;
     }
