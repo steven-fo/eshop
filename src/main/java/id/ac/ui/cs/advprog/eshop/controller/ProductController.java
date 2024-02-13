04510359310bf5835bdd1048257a3ac658cf918c
steven-fo <stevenfo0711@gmail.com>
2024-02-14 01:22:09 +0700

    fix: change page redirect to case sensitive

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -24,7 +24,7 @@ public class ProductController {
     public String createProductPage(Model model) {
         Product product = new Product();
         model.addAttribute("product", product);
-        return "createProduct";
+        return "CreateProduct";
     }
 
     @PostMapping("/create")
@@ -37,14 +37,14 @@ public class ProductController {
     public String productListPage(Model model) {
         List<Product> allProducts = service.findAll();
         model.addAttribute("products", allProducts);
-        return "productList";
+        return "ProductList";
     }
 
     @GetMapping("/edit/{id}")
     public String editProductPage(@PathVariable("id") String id, Model model) {
         Product product = service.findById(Integer.parseInt(id));
         model.addAttribute("product", product);
-        return "editProduct";
+        return "EditProduct";
     }
 
     @PutMapping("/edit")
