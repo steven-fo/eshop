2d748c075c5d8d4a2dce03d01b35bc019ab59015
steven-fo <stevenfo0711@gmail.com>
2024-02-13 22:07:24 +0700

    feat: add product service test

--- src/test/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImplTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImplTest.java
@@ -1,2 +1,79 @@
-package id.ac.ui.cs.advprog.eshop.service;public class ProductServiceImplTest {
+package id.ac.ui.cs.advprog.eshop.service;
+
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import id.ac.ui.cs.advprog.eshop.repository.ProductRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
+class ProductServiceImplTest {
+
+    @Mock
+    private ProductRepository productRepository;
+
+    @InjectMocks
+    private ProductServiceImpl productService;
+
+    @Test
+    public void testCreate() {
+        Product product = new Product();
+
+        productService.create(product);
+
+        verify(productRepository, times(1)).create(product);
+    }
+
+    @Test
+    public void testFindAll() {
+        List<Product> products = new ArrayList<>();
+        products.add(new Product());
+        products.add(new Product());
+        Iterator<Product> iterator = products.iterator();
+        when(productRepository.findAll()).thenReturn(iterator);
+
+        List<Product> result = productService.findAll();
+
+        assertEquals(2, result.size());
+    }
+
+    @Test
+    public void testFindById() {
+        long id = 1L;
+        Product expectedProduct = new Product();
+        when(productRepository.findById(id)).thenReturn(expectedProduct);
+
+        Product result = productService.findById(id);
+
+        assertEquals(expectedProduct, result);
+    }
+
+    @Test
+    public void testEdit() {
+        Product currentProduct = new Product();
+        Product newProduct = new Product();
+
+        productService.edit(currentProduct, newProduct);
+
+        verify(productRepository, times(1)).edit(currentProduct, newProduct);
+    }
+
+    @Test
+    public void testDelete() {
+        Product product = new Product();
+
+        productService.delete(product);
+
+        verify(productRepository, times(1)).delete(product);
+    }
 }
+
