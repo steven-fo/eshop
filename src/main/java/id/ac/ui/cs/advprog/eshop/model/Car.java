05b9e01cd3c9bf2e0ee65682453f9fc877ec5c63
steven-fo <stevenfo0711@gmail.com>
2024-02-15 09:22:19 +0700

    feat: add car model

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Car.java
@@ -0,0 +1,11 @@
+package id.ac.ui.cs.advprog.eshop.model;
+import lombok.Setter;
+import lombok.Getter;
+
+@Setter @Getter
+public class Car {
+    private String carId;
+    private String carName;
+    private String carColor;
+    private int carQuantity;
+}
