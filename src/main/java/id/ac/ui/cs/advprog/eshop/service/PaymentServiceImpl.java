c257e0d408b0674437342ba846ea09eb58c7db07
steven-fo <stevenfo0711@gmail.com>
2024-03-06 20:58:50 +0700

    [GREEN] Implement payment service

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
@@ -1,2 +1,49 @@
-package id.ac.ui.cs.advprog.eshop.service;public class PaymentServiceImpl {
+package id.ac.ui.cs.advprog.eshop.service;
+
+import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
+import id.ac.ui.cs.advprog.eshop.model.Order;
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+import id.ac.ui.cs.advprog.eshop.repository.OrderRepository;
+
+import id.ac.ui.cs.advprog.eshop.repository.PaymentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+    @Autowired
+    private PaymentRepository paymentRepository;
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Override
+    public Payment addPayment(Order order, String method, Map<String, String> paymentDetails) {
+        Payment payment = new Payment(order.getId(), method, paymentDetails);
+        paymentRepository.save(payment);
+        return payment;
+    }
+
+    @Override
+    public Payment setStatus(Payment payment, String status) {
+        Order order = orderRepository.findById(payment.getId());
+        if (order != null) {
+            if (status.equals("SUCCESS")) {
+                order.setStatus(OrderStatus.SUCCESS.getValue());
+            } else if (status.equals("REJECTED")) {
+                order.setStatus(OrderStatus.FAILED.getValue());
+            } else {
+                throw new IllegalArgumentException();
+            }
+            orderRepository.save(order);
+            payment.setStatus(status);
+            paymentRepository.save(payment);
+        } else {
+            throw new NoSuchElementException();
+        }
+        return payment;
+    }
 }
